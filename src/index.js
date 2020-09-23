6cc906f2558926e7d2164bf0773a75f2c9cb37bb
MARCOS DE OLIVEIRA CORREA <marcosocorrea@gmail.com>
2020-09-23 19:22:32 -0300

    parte grafica
    parte grafica

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
